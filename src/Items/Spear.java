21946de8e9374c9b948c3c5b2be90f4e4192384e
Alexandro <105998036+Eternalred05@users.noreply.github.com>
2025-12-23 02:21:48 -0500

    Delete src/Items directory

--- src/Items/Spear.java
+++
@@ -1,9 +0,0 @@
-
-package Items;
-
-public class Spear extends Weapon{
-     public Spear(String info, String name, String id, int attack, int lifeSpan,
-            String effect, String type){
-        super(info, name, id, attack, lifeSpan, effect, type);    
-     }
-}
--- src/Items/Sword.java
+++
@@ -1,8 +0,0 @@
-package Items;
-
-public class Sword extends Weapon{
-    public Sword(String info, String name, String id, int attack, int lifeSpan,
-            String effect, String type){
-        super(info, name, id, attack, lifeSpan, effect, type);
-    }
-}
