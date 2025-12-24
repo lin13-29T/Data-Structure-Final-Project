8db26547cab9779fd1cee1a142183c7166ef8f7e
Alexandro <105998036+Eternalred05@users.noreply.github.com>
2025-12-23 23:25:33 -0500

    Delete Items directory

--- Items/Wand.java
+++
@@ -1,9 +0,0 @@
-package Items;
-
-public class Wand extends Weapon {
-
-    public Wand(String info, String name, String id, int attack, int lifeSpan, String effect) {
-        super(info, name, id, attack, lifeSpan, effect);
-    }
-
-}
